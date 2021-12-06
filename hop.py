819cc129a26763bfdfd70ecc89a47018310df180
Mantalstudio <75010285+Mantalstudio@users.noreply.github.com>
2021-12-06 11:53:24 +0500

    Update hop.py

--- hop.py
+++ hop.py
@@ -18,7 +18,7 @@ else:
     x = str(struct.calcsize("P") * 8)
     distro = subprocess.check_output('uname -om', shell=True)
     android_version = subprocess.check_output('getprop ro.build.version.release', shell=True)
-    if '5' in android_version:
+    if '2' in android_version:
         print('   Your device may not be supported')
         os.sys.exit()
     else:
